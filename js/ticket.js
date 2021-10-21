31d466cfeb5af6db265db9a9df574cf45d5d1459
GianniPio <giannipiopellegrino00@gmail.com>
2021-10-21 16:17:23 +0200

    calculeted sale

--- js/ticket.js
+++ js/ticket.js
@@ -5,15 +5,31 @@
 // va applicato uno sconto del 40% per gli over 65.
 // L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
+// variabili generali
+
+const prezzoKm = 0.21;
+let prezzoTicket = document.getElementById("calcualated_ticket");
+
 // chiedere all'utente quanti kilometri deve percorrere
 
 const userKm = prompt("Inserire quanti Kilometri devi percorrere: ");
-
 // chiedere all'utente quanti anni ha
 const userAge = prompt("Inserisci la tua età (per un possibile sconto): ");
 
 // calcolare il prezzo del biglietto (senza sconto)
+prezzoTicket = userKm * prezzoKm;
 
 // applicare lo sconto
+
+let scontoKm;
+
+if (userAge < 18 ) {
+    scontoKm = prezzoTicket * 0.20;
+    prezzoTicket = prezzoTicket - scontoKm;
+} else if (userAge >= 65) {
+    scontoKm = prezzoTicket * 0.40;
+    prezzoTicket = prezzoTicket - scontoKm;
+}
+
 
 // prezzo finale
